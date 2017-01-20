027a1f0c3108c230b0398673ec48351cbed10768
David Hemming <dhemming@odecee.com.au>
2017-01-20 16:45:29 +1100

    Added hello world test case.

--- reservation-service/src/test/java/com/example/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/example/ReservationServiceApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReservationServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test public void helloWorldTest() {
+
+		assertThat("Hello World").isEqualTo("Hello World");
 	}
 
 }
